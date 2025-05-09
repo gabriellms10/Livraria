bd1801bc3b507377061fa7fbd57be24b985c8112
ALPHA - ADS <ALPHA - ADS@DESKTOP-9LK3UQP>
2025-05-08 21:34:58 -0300

    Atualização no LivroController

--- livraria/src/main/java/gabriel/livraria/controller/LivroController.java
+++ livraria/src/main/java/gabriel/livraria/controller/LivroController.java
@@ -3,24 +3,38 @@ package gabriel.livraria.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import gabriel.livraria.entity.Livro;
+import gabriel.livraria.exceptions.EntidadeNaoEncontradaExcepition;
 import gabriel.livraria.service.LivroService;
 
 @RestController
 @RequestMapping("/livros")
 public class LivroController {
-	
+
 	@Autowired
 	private LivroService service;
-	
+
 	@GetMapping
 	public List<Livro> listar() {
 		List<Livro> livros = service.listar();
 		return livros;
+	}
+
+	public ResponseEntity<Livro> buscar(@PathVariable Long id) {
+
+		try {
+			Livro livro = service.buscarOuFalhar(id);
+			return ResponseEntity.ok(livro);
+		} catch (EntidadeNaoEncontradaExcepition e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
 	}
 
 }
